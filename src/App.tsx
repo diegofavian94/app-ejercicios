a2850b223f8964b32cdc3cc196cbcebaa5eea5d7
DiegoFavianGarcia <47956053+diegofavian94@users.noreply.github.com>
2023-02-12 18:40:31 -0500

    Router Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css'
 import Home from './components/Home/Home'
 import Main from './components/Main/Main'
-import { Route } from 'wouter'
+import {Router, Route } from 'wouter'
 
 function App() {
   
 
   return (
-    <div className="App">
-      <Route path="/" component={Home} />        
-      <Route path="/main" component={Main} />
-    </div>
+    <Router base="/app-ejercicios">
+      <div className="App">
+        <Route path="/" component={Home} />        
+        <Route path="/main" component={Main} />
+      </div>
+    </Router>
   )
 }
 
